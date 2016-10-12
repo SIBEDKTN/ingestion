dc6b010bbf01074bbaead39da00874512de4085b
Siro Bettio <SIBE@int.sonofon.dk>
2016-10-12 15:50:46 +0200

    Initial commit

---
+++ src/main/java/dk/telenor/analytics/bigdata/App.java
@@ -0,0 +1,13 @@
+package dk.telenor.analytics.bigdata;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
